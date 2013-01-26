c3354048a412c416c3a757bf424983f3b5b73821
Mark Fowler <growler1970@gmail.com>
2013-01-26 16:23:41 +0000

    Added methods to read all divisions and seasons

--- src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
+++ src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
@@ -3,13 +3,19 @@ package uk.co.mindbadger.footballresultsanalyser.dao;
 import java.util.List;
 import java.util.Set;
 
+import uk.co.mindbadger.footballresultsanalyser.domain.Division;
 import uk.co.mindbadger.footballresultsanalyser.domain.Fixture;
 import uk.co.mindbadger.footballresultsanalyser.domain.Season;
 import uk.co.mindbadger.footballresultsanalyser.domain.SeasonDivision;
 import uk.co.mindbadger.footballresultsanalyser.domain.SeasonDivisionTeam;
+import uk.co.mindbadger.footballresultsanalyser.domain.Team;
 
 public interface FootballResultsAnalyserDAO {
     public List<Season> getSeasons ();
+    
+    public List<Division> getAllDivisions ();
+    
+    public List<Team> getAllTeams ();
     
     public Set<SeasonDivision> getDivisionsForSeason (int seasonNumber);
 
