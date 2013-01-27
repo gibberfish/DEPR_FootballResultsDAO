fd86fca58ac5dae602afaea2730e169f1e20556d
Mark Fowler <growler1970@gmail.com>
2013-01-27 14:52:27 +0000

    Removed IDs from the domain object factory

--- src/main/java/uk/co/mindbadger/footballresultsanalyser/domain/DomainObjectFactory.java
+++ src/main/java/uk/co/mindbadger/footballresultsanalyser/domain/DomainObjectFactory.java
@@ -2,9 +2,9 @@ package uk.co.mindbadger.footballresultsanalyser.domain;
 
 public interface DomainObjectFactory {
     public Season createSeason (Integer seasonNumber);
-    public Division createDivision (Integer divisionId, String divisionName);
-    public Team createTeam (Integer teamId, String teamName);
-    public Fixture createFixture (Integer fixtureId, Season season, Team homeTeam, Team awayTeam);
+    public Division createDivision (String divisionName);
+    public Team createTeam (String teamName);
+    public Fixture createFixture (Season season, Team homeTeam, Team awayTeam);
     public SeasonDivision createSeasonDivision (Season season, Division division);
     public SeasonDivisionTeam createSeasonDivisionTeam (SeasonDivision seasonDivision, Team team);
     
