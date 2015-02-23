3399513ebd7f8cbef6cd9f1f0f66d06f2b9bcc02
gibberfish <growler1970@gmail.com>
2015-02-23 14:46:21 +0000

    Remove methods not yet needed and standardise other methods
    Remove methods not yet needed and standardise other methods

--- src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
+++ src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
@@ -13,28 +13,32 @@ import uk.co.mindbadger.footballresultsanalyser.domain.SeasonDivisionTeam;
 import uk.co.mindbadger.footballresultsanalyser.domain.Team;
 
 public interface FootballResultsAnalyserDAO<K> {
+	// Season
     public List<Season<K>> getSeasons ();
     public Season<K> getSeason (Integer seasonNum);
     public Season<K> addSeason (Integer seasonNum);
     
+    // Division
     public Map<K, Division<K>> getAllDivisions ();
     public Division<K> addDivision (String divisionName);
+    public Division<String> getDivision(K divId);
     
+    // Teams
     public Map<K, Team<K>> getAllTeams ();
     public Team<K> addTeam (String teamName);
+    public Team<String> getTeam(K teamId);
     
+    // Fixture
     public Fixture<K> addFixture (Season<K> season, Calendar fixtureDate, Division<K> division, Team<K> homeTeam, Team<K> awayTeam, Integer homeGoals, Integer awayGoals);
-    
-    public Set<SeasonDivision<K>> getDivisionsForSeason (int seasonNumber);
-
-    public Set<SeasonDivisionTeam<K>> getTeamsForDivisionInSeason(int seasonNumber, int divisionId);
-
-    public List<Fixture<K>> getFixturesForTeamInDivisionInSeason(int seasonNumber, int divisionId, int teamId);
-    
+    public List<Fixture<K>> getFixturesForTeamInDivisionInSeason(Season<K> season, Division<K> division, Team<K> team);
     public List<Fixture<K>> getUnplayedFixturesBeforeToday();
-    
     public List<Fixture<K>> getFixturesWithNoFixtureDate ();
-    
+
+    // Season Shape
+    //public Set<SeasonDivision<K>> getDivisionsForSeason (int seasonNumber);
+    //public Set<SeasonDivisionTeam<K>> getTeamsForDivisionInSeason(int seasonNumber, int divisionId);
+
+    // Housekeeping
     public void startSession ();
     public void closeSession ();
 }
