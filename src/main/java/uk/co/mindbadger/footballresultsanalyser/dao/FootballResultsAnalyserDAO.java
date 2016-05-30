117688eac10a09eec5505ef240e5e048522fc8db
gibberfish <growler1970@gmail.com>
2016-05-30 11:31:53 +0100

    Re-introduce methods required by the analyser program

--- src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
+++ src/main/java/uk/co/mindbadger/footballresultsanalyser/dao/FootballResultsAnalyserDAO.java
@@ -33,15 +33,16 @@ public interface FootballResultsAnalyserDAO {
 	public Fixture getFixture (String fixtureId);
     //public Fixture getFixture (Season season, Division division, Team homeTeam, Team awayTeam);
     public List<Fixture> getFixtures ();
-    //public List<Fixture> getFixturesForDivisionInSeason (SeasonDivision seasonDivision);
-    //public List<Fixture> getFixturesForTeamInDivisionInSeason(SeasonDivision seasonDivision, Team team);
+    public List<Fixture> getFixturesForDivisionInSeason (SeasonDivision seasonDivision);
+    public List<Fixture> getFixturesForTeamInDivisionInSeason(SeasonDivision seasonDivision, Team team);
 
     // Season Shape
 	public SeasonDivision addSeasonDivision (Season season, Division division, int position);
 	public SeasonDivisionTeam addSeasonDivisionTeam (SeasonDivision seasonDivision, Team team);
 	public List<SeasonDivision> getDivisionsForSeason (Season season);
 	public List<SeasonDivisionTeam> getTeamsForDivisionInSeason(SeasonDivision seasonDivision);
-
+	public SeasonDivision getSeasonDivision (Season season, Division division);
+	
     // Housekeeping
     public void startSession ();
     public void closeSession ();
